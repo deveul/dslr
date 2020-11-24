d00b17f2f1e8c261ae770cd00efc343eef4fc003
VladimirRenaudin <vladimir.renaudin@hotmail.fr>
2020-11-25 00:33:40 +0100

    3d vizualisation

--- logreg_predict.py
+++ logreg_predict.py
@@ -7,6 +7,7 @@ import json
 import pandas as pd
 import numpy as np
 from pandas.api.types import is_numeric_dtype
+from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
 from matplotlib import pyplot as plt
 from utils.stats_functions import dslr_mean
 from utils.stats_functions import dslr_std
@@ -32,6 +33,7 @@ def is_valid_json_file(parser, arg):
 class Predict:
     def __init__(self, data_file, weights_file):
         self.data_file = data_file
+        self.df = None
         self.gryffindor = None
         self.slytherin = None
         self.ravenclaw = None
@@ -84,6 +86,7 @@ class Predict:
             if is_numeric_dtype(df[column].dtypes):
                 df[column] = df[column].fillna(dslr_mean(df[column].dropna()))
                 df[column] = z_score(df[column])
+        self.df = df
         X = np.array(df)
         X = np.hstack((np.ones((len(X), 1)), X))
         gryf = self.predict(X, self.gryffindor)
@@ -95,13 +98,51 @@ class Predict:
         houses = df.idxmax(axis=1)
         return range(len(houses)), houses
 
+    def get_house_repartition(self, house, dfs):
+        x = dfs['Astronomy'].get_group(house)
+        y = dfs['Herbology'].get_group(house)
+        z = dfs['Ancient Runes'].get_group(house)
+        return x, y, z
+
+    def visualize(self):
+        houses = pd.DataFrame(self.houses, columns=['House'])
+        repartition = pd.concat([houses, self.df], axis=1)
+        dfs = repartition.groupby('House')
+        fig = plt.figure()
+        ax = plt.axes(projection='3d')
+        plt.title("Students repartition")
+
+        xg, yg, zg = self.get_house_repartition("Gryffindor", dfs)
+        xs, ys, zs = self.get_house_repartition("Slytherin", dfs)
+        xr, yr, zr = self.get_house_repartition("Ravenclaw", dfs)
+        xh, yh, zh = self.get_house_repartition("Hufflepuff", dfs)
+
+        ax.scatter(xg, yg, zg, label='Gryffindor',c='r')
+        ax.scatter(xs, ys, zs, label='Slytherin',c='darkgreen')
+        ax.scatter(xr, yr, zr, label='Ravenclaw',c='b')
+        ax.scatter(xh, yh, zh, label='Hufflepuff',c='gold')
+
+        fig.tight_layout()
+        fig.subplots_adjust(right=0.8)
+        
+        ax.legend(loc='center left', bbox_to_anchor=(1.10, 0.5), fontsize=9)
+
+        ax.set_xlabel('\nAstronomy')
+        ax.set_ylabel('\nHerbology')
+        ax.set_zlabel('\nAncient Runes')
+
+        plt.show()
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("data_file", help="the csv file containing the data set", type=lambda x: is_valid_csv_file(parser, x))
     parser.add_argument("weights_file", help="the json file containing the weigths to use for the prediction", type=lambda x: is_valid_json_file(parser, x))
+    parser.add_argument("-v", "--visualize", help="vizualize repartition of the students in 3d", action="store_true")
     args = parser.parse_args()
     predict = Predict(args.data_file, args.weights_file)
     predict.save_values()
+    if args.visualize:
+            predict.visualize()
 
 if __name__ == "__main__":
     main()
